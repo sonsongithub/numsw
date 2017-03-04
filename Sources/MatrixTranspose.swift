17379152d2d4a373386aca943210fd589391cb73
araki <araki@qoncept.co.jp>
2017-03-04 13:40:04 +0900

    Add MatrixTranspose ans its tests

---
+++ Sources/MatrixTranspose.swift
@@ -0,0 +1,15 @@
+
+extension Matrix {
+    func transposed() -> Matrix<T> {
+        let newElements = UnsafeMutablePointer<T>.allocate(capacity: rows*columns)
+        defer { newElements.deallocate(capacity: rows*columns) }
+        for r in 0..<rows {
+            for c in 0..<columns {
+                newElements[c*rows+r] = elements[r*columns+c]
+            }
+        }
+        return Matrix<T>(rows: columns,
+                         columns: rows,
+                         elements: Array(UnsafeBufferPointer(start: newElements, count: rows*columns)))
+    }
+}
