33d8a38debfed068ce4a55ec87e0f68c792f3b74
omochi <omochi.metaru@gmail.com>
2017-03-04 12:03:35 +0900

    RendererDebug wip

---
+++ playgrounds/RendererDebugViewController.swift
@@ -0,0 +1,20 @@
+//
+//  RendererDebugViewController.swift
+//  sandbox
+//
+//  Created by omochimetaru on 2017/03/04.
+//  Copyright © 2017年 sonson. All rights reserved.
+//
+
+import UIKit
+
+class RendererDebugViewController : UIViewController {
+
+    required init() {
+        super.init(nibName: "RendererDebugViewController", bundle: nil)
+    }
+    
+    
+    
+}
+
