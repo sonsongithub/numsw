50544fc910c007a950ff1417ad427c908bf3bb81
araki <araki@qoncept.co.jp>
2017-03-04 12:35:32 +0900

    Add row subscript, SubscriptTests

--- Sources/MatrixSubscript.swift
+++ Sources/MatrixSubscript.swift
@@ -14,7 +14,12 @@ extension Matrix {
             return Matrix(rows: 1, columns: columns, elements: Array(elements[row * columns..<row * columns + columns]))
         }
         set {
-            // TODO
+            precondition(newValue.rows == 1, "Matrix rows must be 1.")
+            precondition(newValue.columns == self.columns, "Matrices must have same columns.")
+            
+            for c in 0..<self.columns {
+                self[row, c] = newValue[0, c]
+            }
         }
     }
 }
---
+++ Tests/numswTests/MatrixSubscriptTests.swift
@@ -0,0 +1,35 @@
+
+import XCTest
+@testable import numsw
+
+class MatrixSubscriptTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testElementSubscript() {
+        var mat = Matrix(rows: 3, columns: 3, elements: Array(0..<9))
+        
+        XCTAssertEqual(mat[0, 0], 0)
+        XCTAssertEqual(mat[0, 1], 1)
+        XCTAssertEqual(mat[1, 0], 3)
+        
+    }
+    
+    func testRowSubscript() {
+        var mat = Matrix(rows: 3, columns: 3, elements: Array(0..<9))
+        
+        XCTAssertEqual(mat[0].elements, [0, 1, 2])
+        
+        mat[0] = Matrix(rows: 1, columns: 3, elements: Array(10..<13))
+        
+        XCTAssertEqual(mat[0].elements, [10, 11, 12])
+    }
+}
