01f690b196948664d255fdc8608e5f0f280e6ede
color_box <colorbox222@gmail.com>
2017-03-04 12:17:32 +0900

    Add RendererViewController

---
+++ sandbox/sandbox/RenderViewController.swift
@@ -0,0 +1,23 @@
+//
+//  RenderViewController.swift
+//  sandbox
+//
+//  Created by color_box on 2017/03/04.
+//  Copyright © 2017年 sonson. All rights reserved.
+//
+
+import UIKit
+
+class RenderViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+
+
+}
