bc5e8c5f958a96a3d123324f4b33bc37bc5d5c85
color_box <colorbox222@gmail.com>
2017-03-04 12:48:45 +0900

    Add RenderViewController with mock

--- sandbox/sandbox/RenderViewController.swift
+++ sandbox/sandbox/RenderViewController.swift
@@ -8,16 +8,52 @@
 
 import UIKit
 
+class Hoge:Renderer{
+    func render(size: CGSize) -> UIImage {
+        return UIImage(named: "SS")!
+    }
+}
+
 class RenderViewController: UIViewController {
+
+
+    var renderers:[Renderer] = []
+
+    @IBOutlet weak var scrollView: UIScrollView!
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        renderers.append(Hoge())
+        renderers.append(Hoge())
+        renderers.append(Hoge())
+        renderers.append(Hoge())
+        renderers.append(Hoge())
+
+    }
+
+
+    override func viewWillAppear(_ animated: Bool) {
+        self.render()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-    
+
+
+    func render(){
+
+        let size = self.view.frame.size
+        // renderer 取り出してscrollviewに追加
+        for renderer in renderers {
+            let imageView = UIImageView(image: renderer.render(size: size))
+            imageView.frame.origin = CGPoint(x: 0, y: scrollView.contentSize.height)
+            scrollView.addSubview(imageView)
+            scrollView.contentSize.height += size.height
+        }
+    }
 
 
 }
