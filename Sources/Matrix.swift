3f129e6d0ef95aa967a6035f1d48483f59b808e9
araki <araki@qoncept.co.jp>
2017-03-04 12:13:15 +0900

    Make methods public

--- Sources/Matrix.swift
+++ Sources/Matrix.swift
@@ -1,11 +1,11 @@
-struct Matrix<T> {
+public struct Matrix<T> {
     
     var rows: Int
     var columns: Int
     
     var elements: [T]
     
-    init(rows: Int, columns: Int, elements: [T]) {
+    public init(rows: Int, columns: Int, elements: [T]) {
         self.rows = rows
         self.columns = columns
         self.elements = elements
@@ -13,7 +13,7 @@ struct Matrix<T> {
 }
 
 extension Matrix {
-    subscript(row: Int, column: Int) -> T {
+    public subscript(row: Int, column: Int) -> T {
         get {
             return elements[row * columns + column]
         }
@@ -22,7 +22,7 @@ extension Matrix {
         }
     }
     
-    subscript(row: Int) -> Matrix<T> {
+    public subscript(row: Int) -> Matrix<T> {
         get {
             return Matrix(rows: 1, columns: columns, elements: Array(elements[row * columns..<row * columns + columns]))
         }
@@ -33,7 +33,7 @@ extension Matrix {
 }
 
 extension Matrix {
-    func reshaped(rows: Int, cols: Int) -> Matrix<T> {
+    public func reshaped(rows: Int, cols: Int) -> Matrix<T> {
         assert(rows * columns == self.rows * self.columns, "Element count must be unchanged.")
         
         return Matrix(rows: rows, columns: columns, elements: self.elements)
---
+++ Sources/MatrixMultiply.swift
@@ -0,0 +1,9 @@
+//
+//  MatrixMultiply.swift
+//  numsw
+//
+//  Created by Araki Takehiro on 2017/03/04.
+//
+//
+
+import Foundation
