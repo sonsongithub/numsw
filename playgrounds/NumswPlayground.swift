f6303ea39d89bf315c0e778376dc56a18a9cdb81
omochi <omochi.metaru@gmail.com>
2017-03-06 02:27:42 +0900

    ChartBuilder and hold API

--- playgrounds/NumswPlayground.swift
+++ playgrounds/NumswPlayground.swift
@@ -19,39 +19,42 @@ public class NumswPlayground {
         renderers.append(renderer)
     }
     
-    public func addLine(
-        x: [Double], y: [Double])
-    {
-        let points = zip(x, y).map {
-            CGPoint(x: $0.0, y:$0.1)
+    public func plot(x: [Double], y: [Double]) {
+        guard let b = chartBuilder else {
+            hold {
+                plot(x: x, y: y)
+            }
+            return
         }
         
-        var chart = Chart()
-        chart.elements.append(.line(LineGraph(points: points)))
-        chart.computeViewport()
-        
-        let renderer = ChartRenderer(chart: chart)
-        append(renderer: renderer)
+        let points = zip(x, y).map {
+            CGPoint(x: $0.0, y: $0.1)
+        }
+        b.addLine(line: LineGraph(points: points))
     }
     
-    public func addLine2(
-            x: [Double], y: [Double],
-            x2: [Double], y2: [Double])
-    {
-        let points1 = zip(x, y).map {
+    public func scatter(x: [Double], y: [Double]) {
+        guard let b = chartBuilder else {
+            hold {
+                scatter(x: x, y: y)
+            }
+            return
+        }
+        let points = zip(x, y).map {
             CGPoint(x: $0.0, y: $0.1)
         }
+        b.addScatter(scatter: ScatterGraph(points: points))
+    }
+    
+    public func hold(_ f: () throws -> Void) rethrows {
+        let b = ChartBuilder()
+        chartBuilder = b
+
+        try f()
         
-        let points2 = zip(x2, y2).map {
-            CGPoint(x: $0.0, y: $0.1)
-        }
+        chartBuilder = nil
         
-        var chart = Chart()
-        chart.elements.append(.line(LineGraph(points: points1)))
-        chart.elements.append(.scatter(ScatterGraph(points: points2)))
-        chart.computeViewport()
-        
-        let renderer = ChartRenderer(chart: chart)
+        let renderer = ChartRenderer(chart: b.chart)
         append(renderer: renderer)
     }
     
@@ -70,20 +73,42 @@ public class NumswPlayground {
         }
     }
     
+    private var chartBuilder: ChartBuilder?
+    
     private static var _shared: NumswPlayground?
+    
 }
 
 public func addLine(
     x: [Double], y: [Double])
 {
-    NumswPlayground.shared.addLine(x: x, y: y)
+    hold {
+        plot(x: x, y: y)
+    }
 }
 
 public func addLine2(
     x: [Double], y: [Double],
     x2: [Double], y2: [Double])
 {
-    NumswPlayground.shared.addLine2(x: x, y: y,
-                                    x2: x2, y2: y2)
+    hold {
+        plot(x: x, y: y)
+        scatter(x: x2, y: y2)
+    }
 }
 
+public func plot(
+    x: [Double], y: [Double])
+{
+    NumswPlayground.shared.plot(x: x, y: y)
+}
+
+public func scatter(
+    x: [Double], y: [Double])
+{
+    NumswPlayground.shared.scatter(x: x, y: y)
+}
+
+public func hold(f: () throws -> Void) rethrows {
+    try NumswPlayground.shared.hold(f)
+}
