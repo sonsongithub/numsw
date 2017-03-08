ac9521b7fe91941d89a07b6f01dbb08f290b14ea
omochi <omochi.metaru@gmail.com>
2017-03-08 17:28:38 +0900

    update book

--- Release/numsw-xcode.playground/Sources/numsw/MatrixAddition.swift
+++ Release/numsw-xcode.playground/Sources/numsw/MatrixAddition.swift
@@ -9,6 +9,8 @@
 import Foundation
 import Accelerate
 
+// MARK: - Addition
+
 public func add(_ lhs: Matrix, rhs: Double) -> Matrix {
     let newElements = lhs.elements.map { (value) -> Double in
         return value + rhs
@@ -28,6 +30,24 @@ public func add(_ lhs: Matrix, rhs: Matrix) -> Matrix {
     
     return Matrix(rows: lhs.rows, columns: lhs.columns, elements: Array(UnsafeBufferPointer(start: cElements, count: lhs.count)))
 }
+
+public func += (lhs: inout Matrix, rhs: Double) {
+    lhs = lhs + rhs
+}
+
+public func += (lhs: inout Matrix, rhs: Matrix) {
+    lhs = lhs + rhs
+}
+
+public func + (lhs: Matrix, rhs: Double) -> Matrix {
+    return add(lhs, rhs: rhs)
+}
+
+public func + (lhs: Matrix, rhs: Matrix) -> Matrix {
+    return add(lhs, rhs: rhs)
+}
+
+// MARK: - Subtraction
 
 public func subtract(_ lhs: Matrix, rhs: Double) -> Matrix {
     let newElements = lhs.elements.map { (value) -> Double in
@@ -49,14 +69,6 @@ public func subtract(_ lhs: Matrix, rhs: Matrix) -> Matrix {
     return Matrix(rows: lhs.rows, columns: lhs.columns, elements: Array(UnsafeBufferPointer(start: cElements, count: lhs.count)))
 }
 
-public func += (lhs: inout Matrix, rhs: Double) {
-    lhs = lhs + rhs
-}
-
-public func += (lhs: inout Matrix, rhs: Matrix) {
-    lhs = lhs + rhs
-}
-
 public func -= (lhs: inout Matrix, rhs: Double) {
     lhs = lhs - rhs
 }
@@ -65,25 +77,19 @@ public func -= (lhs: inout Matrix, rhs: Matrix) {
     lhs = lhs - rhs
 }
 
-public func + (lhs: Matrix, rhs: Double) -> Matrix {
-    return add(lhs, rhs: rhs)
-}
-
-public func + (lhs: Matrix, rhs: Matrix) -> Matrix {
-    return add(lhs, rhs: rhs)
-}
-
-public prefix func - (lhs: Matrix) -> Matrix {
-    let newElements = lhs.elements.map { (value) -> Double in
-        return -value
-    }
-    return Matrix(rows: lhs.rows, columns: lhs.columns, elements: Array(UnsafeBufferPointer(start: newElements, count: lhs.count)))
-}
-
 public func - (lhs: Matrix, rhs: Double) -> Matrix {
     return subtract(lhs, rhs: rhs)
 }
 
 public func - (lhs: Matrix, rhs: Matrix) -> Matrix {
     return subtract(lhs, rhs: rhs)
+}
+
+// MARK: - Prefix
+
+public prefix func - (lhs: Matrix) -> Matrix {
+    let newElements = lhs.elements.map { (value) -> Double in
+        return -value
+    }
+    return Matrix(rows: lhs.rows, columns: lhs.columns, elements: Array(UnsafeBufferPointer(start: newElements, count: lhs.count)))
 }
