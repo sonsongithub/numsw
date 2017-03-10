812347170a174142ae0052d5fa6ac11c0559a914
araki <araki@qoncept.co.jp>
2017-03-10 18:26:02 +0900

    Fix: Invalid generic designations in NDArrayRandom

--- Sources/NDArray/NDArrayRandom.swift
+++ Sources/NDArray/NDArrayRandom.swift
@@ -5,16 +5,19 @@ private func _uniform<T: FloatingPoint>(low: T = 0, high: T = 1) -> T {
     return (high-low)*(T(arc4random_uniform(UInt32.max)) / T(UInt32.max))+low
 }
 
-extension NDArray {
+extension NDArray where T: FloatingPointFunctions & FloatingPoint {
     
-    public static func uniform<T: FloatingPointFunctions & FloatingPoint>(low: T = 0, high: T = 1, shape: [Int]) -> NDArray<T> {
+    public static func uniform(low: T = 0, high: T = 1, shape: [Int]) -> NDArray<T> {
         let count = shape.reduce(1, *)
         let elements = (0..<count).map { _ in _uniform(low: low, high: high) }
         
         return NDArray<T>(shape: shape, elements: elements)
     }
+}
+
+extension NDArray where T: FloatingPoint & FloatingPointFunctions & Arithmetic {
     
-    public static func normal<T: FloatingPoint & FloatingPointFunctions & Arithmetic>(mu: T = 0, sigma: T = 0, shape: [Int]) -> NDArray<T> {
+    public static func normal(mu: T = 0, sigma: T = 0, shape: [Int]) -> NDArray<T> {
         let u1 = uniform(low: T(0), high: T(1), shape: shape)
         let u2 = uniform(low: T(0), high: T(1), shape: shape)
         
@@ -22,5 +25,4 @@ extension NDArray {
         
         return stdNormal*sigma + mu
     }
-    
 }
