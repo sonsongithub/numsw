56afe4e262785c9f56535df77d7e2fe14114773f
araki <araki@qoncept.co.jp>
2017-03-10 17:26:34 +0900

    Change NDArrayBoolTests

--- Tests/numswTests/NDArrayTests/NDArrayBoolTests.swift
+++ Tests/numswTests/NDArrayTests/NDArrayBoolTests.swift
@@ -8,25 +8,25 @@ class NDArrayBoolTests: XCTestCase {
         let a = NDArray(shape: [2, 2], elements: [true, false, true, false])
         let b = NDArray(shape: [2, 2], elements: [true, true, false, false])
         
-        XCTAssertEqual(!a, NDArray(shape: [2, 2], elements: [false, true, false, true]))
-        XCTAssertEqual(a && b, NDArray(shape: [2, 2], elements: [true, false, false, false]))
-        XCTAssertEqual(a || b, NDArray(shape: [2, 2], elements: [true, true, true, false]))
+        XCTAssertEqual(not(a), NDArray(shape: [2, 2], elements: [false, true, false, true]))
+        XCTAssertEqual(and(a, b), NDArray(shape: [2, 2], elements: [true, false, false, false]))
+        XCTAssertEqual(or(a, b), NDArray(shape: [2, 2], elements: [true, true, true, false]))
     }
     
     func testCompare() {
         let a = NDArray(shape: [2, 2], elements: [1, 2, 3, 4])
         let b = NDArray(shape: [2, 2], elements: [1, 3, 2, 4])
         
-        XCTAssertEqual(a == 2, NDArray(shape: [2, 2], elements: [false, true, false, false]))
-        XCTAssertEqual(a < 2, NDArray(shape: [2, 2], elements: [true, false, false, false]))
-        XCTAssertEqual(a > 3, NDArray(shape: [2, 2], elements: [false, false, false, true]))
-        XCTAssertEqual(a <= 2, NDArray(shape: [2, 2], elements: [true, true, false, false]))
-        XCTAssertEqual(a >= 3, NDArray(shape: [2, 2], elements: [false, false, true, true]))
+        XCTAssertEqual(equal(a, 2), NDArray(shape: [2, 2], elements: [false, true, false, false]))
+        XCTAssertEqual(lessThan(a, 2), NDArray(shape: [2, 2], elements: [true, false, false, false]))
+        XCTAssertEqual(greaterThan(a, 3), NDArray(shape: [2, 2], elements: [false, false, false, true]))
+        XCTAssertEqual(notGreaterThan(a, 2), NDArray(shape: [2, 2], elements: [true, true, false, false]))
+        XCTAssertEqual(notLessThan(a, 3), NDArray(shape: [2, 2], elements: [false, false, true, true]))
         
-        XCTAssertEqual(a == b, NDArray(shape: [2, 2], elements: [true, false, false, true]))
-        XCTAssertEqual(a < b, NDArray(shape: [2, 2], elements: [false, true, false, false]))
-        XCTAssertEqual(a > b, NDArray(shape: [2, 2], elements: [false, false, true, false]))
-        XCTAssertEqual(a <= b, NDArray(shape: [2, 2], elements: [true, true, false, true]))
-        XCTAssertEqual(a >= b, NDArray(shape: [2, 2], elements: [true, false, true, true]))
+        XCTAssertEqual(equal(a, b), NDArray(shape: [2, 2], elements: [true, false, false, true]))
+        XCTAssertEqual(lessThan(a, b), NDArray(shape: [2, 2], elements: [false, true, false, false]))
+        XCTAssertEqual(greaterThan(a, b), NDArray(shape: [2, 2], elements: [false, false, true, false]))
+        XCTAssertEqual(notGreaterThan(a, b), NDArray(shape: [2, 2], elements: [true, true, false, true]))
+        XCTAssertEqual(notLessThan(a, b), NDArray(shape: [2, 2], elements: [true, false, true, true]))
     }
 }
