ef215be47e15d5480929127fe283577828c58476
araki <araki@qoncept.co.jp>
2017-03-10 18:20:56 +0900

    Rewrite Bool operators with pointer

--- Sources/NDArray/NDArrayBool.swift
+++ Sources/NDArray/NDArrayBool.swift
@@ -60,70 +60,244 @@ public func >=<T: Comparable>(lhs: NDArray<T>, rhs: NDArray<T>) -> NDArray<Bool>
 
 // unary
 func not(_ arg: NDArray<Bool>) -> NDArray<Bool> {
-    return NDArray(shape: arg.shape, elements: arg.elements.map(!))
+    var inPointer = UnsafePointer(arg.elements)
+    let outPointer = UnsafeMutablePointer<Bool>.allocate(capacity: arg.elements.count)
+    defer { outPointer.deallocate(capacity: arg.elements.count) }
+    
+    var p = outPointer
+    for _ in 0..<arg.elements.count {
+        p.pointee = !inPointer.pointee
+        p += 1
+        inPointer += 1
+    }
+    
+    return NDArray(shape: arg.shape,
+                   elements: Array(UnsafeBufferPointer(start: outPointer, count: arg.elements.count)))
+
 }
 
+// binary
 func and(_ lhs: NDArray<Bool>, _ rhs: NDArray<Bool>) -> NDArray<Bool> {
     precondition(lhs.shape==rhs.shape, "Two NDArrays have incompatible shape.")
     
-    let elements = zip(lhs.elements, rhs.elements).map { $0 && $1 }
+    var lhsPointer = UnsafePointer(lhs.elements)
+    var rhsPointer = UnsafePointer(rhs.elements)
+    let outPointer = UnsafeMutablePointer<Bool>.allocate(capacity: lhs.elements.count)
+    defer { outPointer.deallocate(capacity: lhs.elements.count) }
     
-    return NDArray(shape: lhs.shape, elements: elements)
+    var p = outPointer
+    for _ in 0..<rhs.elements.count {
+        p.pointee = lhsPointer.pointee && rhsPointer.pointee
+        p += 1
+        lhsPointer += 1
+        rhsPointer += 1
+    }
+    
+    return NDArray(shape: lhs.shape,
+                   elements: Array(UnsafeBufferPointer(start: outPointer, count: lhs.elements.count)))
+
 }
 
 func or(_ lhs: NDArray<Bool>, _ rhs: NDArray<Bool>) -> NDArray<Bool> {
     precondition(lhs.shape==rhs.shape, "Two NDArrays have incompatible shape.")
     
-    let elements = zip(lhs.elements, rhs.elements).map { $0 || $1 }
+    var lhsPointer = UnsafePointer(lhs.elements)
+    var rhsPointer = UnsafePointer(rhs.elements)
+    let outPointer = UnsafeMutablePointer<Bool>.allocate(capacity: lhs.elements.count)
+    defer { outPointer.deallocate(capacity: lhs.elements.count) }
     
-    return NDArray(shape: lhs.shape, elements: elements)
+    var p = outPointer
+    for _ in 0..<rhs.elements.count {
+        p.pointee = lhsPointer.pointee || rhsPointer.pointee
+        p += 1
+        lhsPointer += 1
+        rhsPointer += 1
+    }
+    
+    return NDArray(shape: lhs.shape,
+                   elements: Array(UnsafeBufferPointer(start: outPointer, count: lhs.elements.count)))
 }
 
 
 // NDArray and scalar
 func equal<T: Equatable>(_ lhs: NDArray<T>, _ rhs: T) -> NDArray<Bool> {
-    return NDArray(shape: lhs.shape, elements: lhs.elements.map { $0 == rhs })
+    var inPointer = UnsafePointer(lhs.elements)
+    let outPointer = UnsafeMutablePointer<Bool>.allocate(capacity: lhs.elements.count)
+    defer { outPointer.deallocate(capacity: lhs.elements.count) }
+    
+    var p = outPointer
+    for _ in 0..<lhs.elements.count {
+        p.pointee = inPointer.pointee == rhs
+        p += 1
+        inPointer += 1
+    }
+    
+    return NDArray(shape: lhs.shape,
+                   elements: Array(UnsafeBufferPointer(start: outPointer, count: lhs.elements.count)))
 }
 
 func lessThan<T: Comparable>(_ lhs: NDArray<T>, _ rhs: T) -> NDArray<Bool> {
-    return NDArray(shape: lhs.shape, elements: lhs.elements.map { $0 < rhs })
+    var inPointer = UnsafePointer(lhs.elements)
+    let outPointer = UnsafeMutablePointer<Bool>.allocate(capacity: lhs.elements.count)
+    defer { outPointer.deallocate(capacity: lhs.elements.count) }
+    
+    var p = outPointer
+    for _ in 0..<lhs.elements.count {
+        p.pointee = inPointer.pointee < rhs
+        p += 1
+        inPointer += 1
+    }
+    
+    return NDArray(shape: lhs.shape,
+                   elements: Array(UnsafeBufferPointer(start: outPointer, count: lhs.elements.count)))
 }
 
 func greaterThan<T: Comparable>(_ lhs: NDArray<T>, _ rhs: T) -> NDArray<Bool> {
-    return NDArray(shape: lhs.shape, elements: lhs.elements.map { $0 > rhs })
+    var inPointer = UnsafePointer(lhs.elements)
+    let outPointer = UnsafeMutablePointer<Bool>.allocate(capacity: lhs.elements.count)
+    defer { outPointer.deallocate(capacity: lhs.elements.count) }
+    
+    var p = outPointer
+    for _ in 0..<lhs.elements.count {
+        p.pointee = inPointer.pointee > rhs
+        p += 1
+        inPointer += 1
+    }
+    
+    return NDArray(shape: lhs.shape,
+                   elements: Array(UnsafeBufferPointer(start: outPointer, count: lhs.elements.count)))
 }
 
 func notGreaterThan<T: Comparable>(_ lhs: NDArray<T>, _ rhs: T) -> NDArray<Bool> {
-    return NDArray(shape: lhs.shape, elements: lhs.elements.map { $0 <= rhs })
+    var inPointer = UnsafePointer(lhs.elements)
+    let outPointer = UnsafeMutablePointer<Bool>.allocate(capacity: lhs.elements.count)
+    defer { outPointer.deallocate(capacity: lhs.elements.count) }
+    
+    var p = outPointer
+    for _ in 0..<lhs.elements.count {
+        p.pointee = inPointer.pointee <= rhs
+        p += 1
+        inPointer += 1
+    }
+    
+    return NDArray(shape: lhs.shape,
+                   elements: Array(UnsafeBufferPointer(start: outPointer, count: lhs.elements.count)))
 }
 
 func notLessThan<T: Comparable>(_ lhs: NDArray<T>, _ rhs: T) -> NDArray<Bool> {
-    return NDArray(shape: lhs.shape, elements: lhs.elements.map { $0 >= rhs })
+    var inPointer = UnsafePointer(lhs.elements)
+    let outPointer = UnsafeMutablePointer<Bool>.allocate(capacity: lhs.elements.count)
+    defer { outPointer.deallocate(capacity: lhs.elements.count) }
+    
+    var p = outPointer
+    for _ in 0..<lhs.elements.count {
+        p.pointee = inPointer.pointee >= rhs
+        p += 1
+        inPointer += 1
+    }
+    
+    return NDArray(shape: lhs.shape,
+                   elements: Array(UnsafeBufferPointer(start: outPointer, count: lhs.elements.count)))
 }
 
 
 // NDArray and NDArray
 func equal<T: Equatable>(_ lhs: NDArray<T>, _ rhs: NDArray<T>) -> NDArray<Bool> {
     precondition(lhs.shape == rhs.shape, "")
-    return NDArray(shape: lhs.shape, elements: zip(lhs.elements, rhs.elements).map { $0 == $1 })
+    
+    var lhsPointer = UnsafePointer(lhs.elements)
+    var rhsPointer = UnsafePointer(rhs.elements)
+    let outPointer = UnsafeMutablePointer<Bool>.allocate(capacity: lhs.elements.count)
+    defer { outPointer.deallocate(capacity: lhs.elements.count) }
+    
+    var p = outPointer
+    for _ in 0..<rhs.elements.count {
+        p.pointee = lhsPointer.pointee == rhsPointer.pointee
+        p += 1
+        lhsPointer += 1
+        rhsPointer += 1
+    }
+    
+    return NDArray(shape: lhs.shape,
+                   elements: Array(UnsafeBufferPointer(start: outPointer, count: lhs.elements.count)))
 }
 
 func lessThan<T: Comparable>(_ lhs: NDArray<T>, _ rhs: NDArray<T>) -> NDArray<Bool> {
     precondition(lhs.shape == rhs.shape, "")
-    return NDArray(shape: lhs.shape, elements: zip(lhs.elements, rhs.elements).map { $0 < $1 })
+    
+    var lhsPointer = UnsafePointer(lhs.elements)
+    var rhsPointer = UnsafePointer(rhs.elements)
+    let outPointer = UnsafeMutablePointer<Bool>.allocate(capacity: lhs.elements.count)
+    defer { outPointer.deallocate(capacity: lhs.elements.count) }
+    
+    var p = outPointer
+    for _ in 0..<rhs.elements.count {
+        p.pointee = lhsPointer.pointee < rhsPointer.pointee
+        p += 1
+        lhsPointer += 1
+        rhsPointer += 1
+    }
+    
+    return NDArray(shape: lhs.shape,
+                   elements: Array(UnsafeBufferPointer(start: outPointer, count: lhs.elements.count)))
 }
 
 func greaterThan<T: Comparable>(_ lhs: NDArray<T>, _ rhs: NDArray<T>) -> NDArray<Bool> {
     precondition(lhs.shape == rhs.shape, "")
-    return NDArray(shape: lhs.shape, elements: zip(lhs.elements, rhs.elements).map { $0 > $1 })
+    
+    var lhsPointer = UnsafePointer(lhs.elements)
+    var rhsPointer = UnsafePointer(rhs.elements)
+    let outPointer = UnsafeMutablePointer<Bool>.allocate(capacity: lhs.elements.count)
+    defer { outPointer.deallocate(capacity: lhs.elements.count) }
+    
+    var p = outPointer
+    for _ in 0..<rhs.elements.count {
+        p.pointee = lhsPointer.pointee > rhsPointer.pointee
+        p += 1
+        lhsPointer += 1
+        rhsPointer += 1
+    }
+    
+    return NDArray(shape: lhs.shape,
+                   elements: Array(UnsafeBufferPointer(start: outPointer, count: lhs.elements.count)))
 }
 
 func notGreaterThan<T: Comparable>(_ lhs: NDArray<T>, _ rhs: NDArray<T>) -> NDArray<Bool> {
     precondition(lhs.shape == rhs.shape, "")
-    return NDArray(shape: lhs.shape, elements: zip(lhs.elements, rhs.elements).map { $0 <= $1 })
+    
+    var lhsPointer = UnsafePointer(lhs.elements)
+    var rhsPointer = UnsafePointer(rhs.elements)
+    let outPointer = UnsafeMutablePointer<Bool>.allocate(capacity: lhs.elements.count)
+    defer { outPointer.deallocate(capacity: lhs.elements.count) }
+    
+    var p = outPointer
+    for _ in 0..<rhs.elements.count {
+        p.pointee = lhsPointer.pointee <= rhsPointer.pointee
+        p += 1
+        lhsPointer += 1
+        rhsPointer += 1
+    }
+    
+    return NDArray(shape: lhs.shape,
+                   elements: Array(UnsafeBufferPointer(start: outPointer, count: lhs.elements.count)))
 }
 
 func notLessThan<T: Comparable>(_ lhs: NDArray<T>, _ rhs: NDArray<T>) -> NDArray<Bool> {
     precondition(lhs.shape == rhs.shape, "")
-    return NDArray(shape: lhs.shape, elements: zip(lhs.elements, rhs.elements).map { $0 >= $1 })
+    
+    var lhsPointer = UnsafePointer(lhs.elements)
+    var rhsPointer = UnsafePointer(rhs.elements)
+    let outPointer = UnsafeMutablePointer<Bool>.allocate(capacity: lhs.elements.count)
+    defer { outPointer.deallocate(capacity: lhs.elements.count) }
+    
+    var p = outPointer
+    for _ in 0..<rhs.elements.count {
+        p.pointee = lhsPointer.pointee >= rhsPointer.pointee
+        p += 1
+        lhsPointer += 1
+        rhsPointer += 1
+    }
+    
+    return NDArray(shape: lhs.shape,
+                   elements: Array(UnsafeBufferPointer(start: outPointer, count: lhs.elements.count)))
 }
