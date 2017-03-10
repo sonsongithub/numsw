4ef0fbe567e26101d459f75a6f1b7024e129b221
araki <araki@qoncept.co.jp>
2017-03-10 16:14:38 +0900

    Add NDArrayStack

---
+++ Sources/NDArray/NDArrayStack.swift
@@ -0,0 +1,64 @@
+
+import Foundation
+
+extension NDArray {
+    public static func stack(_ arrays: [NDArray<T>], axis: Int = 0) -> NDArray<T> {
+        let shape = arrays.first!.shape
+        precondition(!arrays.contains { $0.shape != shape }, "All NDArrays must have same shape.")
+        
+        var newShape = shape
+        newShape.insert(1, at: axis)
+        
+        return _concatenate(arrays.map{ $0.reshaped(newShape) }, axis: axis)
+    }
+    
+    public static func concatenate(_ arrays: [NDArray<T>], axis: Int = 0) -> NDArray<T> {
+        let shapeWithoutConcatAxis = arrays.first!.shape.removed(at: axis)
+        precondition(!arrays.contains { $0.shape.removed(at: axis) != shapeWithoutConcatAxis },
+                     "All NDArrays must have same shape.")
+        
+        return _concatenate(arrays, axis: axis)
+    }
+}
+
+extension Array {
+    fileprivate func removed(at index: Int) -> Array {
+        var array = self
+        array.remove(at: index)
+        return array
+    }
+    
+    fileprivate func replaced(with newElement: Element, at index: Int) -> Array {
+        var array = self
+        array[index] = newElement
+        return array
+    }
+}
+
+private func _concatenate<T>(_ arrays: [NDArray<T>], axis: Int) -> NDArray<T> {
+    let shapeBeforeConcatAxis = [Int](arrays.first!.shape.prefix(upTo: axis))
+    let shapeAfterConcatAxis = [Int](arrays.first!.shape.dropFirst(axis+1))
+    
+    let totalCount = arrays.map { $0.elements.count }.reduce(0, +)
+    let concatAxisSize = arrays.map { $0.shape[axis] }.reduce(0, +)
+    
+    let out = UnsafeMutablePointer<T>.allocate(capacity: totalCount)
+    defer { out.deallocate(capacity: totalCount) }
+    
+    let reshapedArrays = arrays.map { $0.reshaped([-1, $0.shape[axis]] + shapeAfterConcatAxis) }
+    let copyCounts = reshapedArrays.map { $0.shape.dropFirst().reduce(1, *) }
+    
+    var pointer = out
+    
+    for i in 0..<reshapedArrays.first!.shape[0] {
+        for (count, array) in zip(copyCounts, reshapedArrays) {
+            let p = UnsafePointer(array.elements).advanced(by: i*count)
+            memcpy(pointer, p, MemoryLayout<T>.size * count)
+            pointer += count
+        }
+    }
+    
+    let elements = Array(UnsafeBufferPointer(start: out, count: totalCount))
+    return NDArray(shape: shapeBeforeConcatAxis + [concatAxisSize] + shapeAfterConcatAxis,
+                   elements: elements)
+}
---
+++ Tests/numswTests/NDArrayTests/NDArrayStackTests.swift
@@ -0,0 +1,78 @@
+
+import XCTest
+@testable import numsw
+
+class NDArrayStackTests: XCTestCase {
+    
+    func testConcatenate() {
+        do {
+            // axis 0
+            let a = NDArray(shape: [2, 2], elements: [1, 2, 3, 4])
+            let b = NDArray(shape: [3, 2], elements: [1, 2, 3, 4, 5, 6])
+            
+            XCTAssertEqual(NDArray.concatenate([a, b], axis: 0),
+                           NDArray(shape: [5, 2], elements: [1, 2, 3, 4, 1, 2, 3, 4, 5, 6]))
+        }
+        do {
+            // axis 1
+            let a = NDArray(shape: [2, 2], elements: [1, 2, 3, 4])
+            let b = NDArray(shape: [2, 3], elements: [1, 2, 3, 4, 5, 6])
+            
+            XCTAssertEqual(NDArray.concatenate([a, b], axis: 1),
+                           NDArray(shape: [2, 5], elements: [1, 2, 1, 2, 3, 3, 4, 4, 5, 6]))
+        }
+        
+        do {
+            let a = NDArray<Int>.range(from: 0, to: 2*3*4, stride: 1).reshaped([2, 3, 4])
+            
+            XCTAssertEqual(NDArray.concatenate([a, a, a], axis: 0),
+                           NDArray(shape: [6, 3, 4],
+                                   elements: [0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12,
+                                              13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23,
+                                              0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12,
+                                              13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23,
+                                              0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12,
+                                              13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]))
+            XCTAssertEqual(NDArray.concatenate([a, a, a], axis: 1),
+                           NDArray(shape: [2, 9, 4],
+                                   elements: [0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11,
+                                              0,  1,  2,  3,  4, 5,  6,  7,  8,  9, 10, 11,
+                                              0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11,
+                                              12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23,
+                                              12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23,
+                                              12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]))
+            
+            XCTAssertEqual(NDArray.concatenate([a, a, a], axis: 2),
+                           NDArray(shape: [2, 3, 12],
+                                   elements: [0,  1,  2,  3,  0,  1,  2,  3,  0,  1,  2,  3,
+                                              4,  5,  6,  7,  4, 5,  6,  7,  4,  5,  6,  7,
+                                              8,  9, 10, 11,  8,  9, 10, 11,  8,  9, 10, 11,
+                                              12, 13, 14, 15, 12, 13, 14, 15, 12, 13, 14, 15,
+                                              16, 17, 18, 19, 16, 17, 18, 19, 16, 17, 18, 19,
+                                              20, 21, 22, 23, 20, 21, 22, 23, 20, 21, 22, 23]))
+        }
+    }
+    
+    func testStack() {
+        do {
+            let a = NDArray<Int>.range(from: 0, to: 12, stride: 1).reshaped([3, 4])
+            
+            XCTAssertEqual(NDArray.stack([a, a], axis: 0),
+                           NDArray(shape: [2, 3, 4],
+                                   elements: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,
+                                              0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]))
+            
+            XCTAssertEqual(NDArray.stack([a, a], axis: 1),
+                           NDArray(shape: [3, 2, 4],
+                                   elements: [0,  1,  2,  3,  0,  1,  2,  3,
+                                              4,  5,  6,  7,  4,  5,  6,  7,
+                                              8, 9, 10, 11,  8,  9, 10, 11]))
+            
+            XCTAssertEqual(NDArray.stack([a, a], axis: 2),
+                           NDArray(shape: [3, 4, 2],
+                                   elements: [0,  0,  1,  1,  2,  2,  3,  3,  4,  4,  5,  5,
+                                              6,  6, 7,  7,  8, 8,  9,  9, 10, 10, 11, 11]))
+            
+        }
+    }
+}
