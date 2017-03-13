a509c0f829fe5f7e0ac07bb16a1e2241ad28882c
araki <araki@qoncept.co.jp>
2017-03-13 16:55:31 +0900

    Make MatrixTranspose generic

--- Sources/numsw/Matrix/MatrixTranspose.swift
+++ Sources/numsw/Matrix/MatrixTranspose.swift
@@ -1,14 +1,17 @@
-//extension Matrix {
-//    public func transposed() -> Matrix {
-//        let newElements = UnsafeMutablePointer<Double>.allocate(capacity: rows*columns)
-//        defer { newElements.deallocate(capacity: rows*columns) }
-//        for r in 0..<rows {
-//            for c in 0..<columns {
-//                newElements[c*rows+r] = elements[r*columns+c]
-//            }
-//        }
-//        return Matrix(rows: columns,
-//                      columns: rows,
-//                      elements: Array(UnsafeBufferPointer(start: newElements, count: rows*columns)))
-//    }
-//}
+
+import Foundation
+
+extension Matrix {
+    public func transposed() -> Matrix<T> {
+        let newElements = UnsafeMutablePointer<T>.allocate(capacity: rows*columns)
+        defer { newElements.deallocate(capacity: rows*columns) }
+        for r in 0..<rows {
+            for c in 0..<columns {
+                newElements[c*rows+r] = elements[r*columns+c]
+            }
+        }
+        return Matrix(rows: columns,
+                      columns: rows,
+                      elements: Array(UnsafeBufferPointer(start: newElements, count: rows*columns)))
+    }
+}
