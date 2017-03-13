f9891b9351b5cf068fe528c08bf987a4fc0f2fee
araki <araki@qoncept.co.jp>
2017-03-13 18:11:14 +0900

    Fix: reshape

--- Sources/numsw/Matrix/MatrixTransformation.swift
+++ Sources/numsw/Matrix/MatrixTransformation.swift
@@ -1,6 +1,6 @@
 
 extension Matrix {
-    public func reshaped(rows: Int, cols: Int) -> Matrix<T> {
+    public func reshaped(rows: Int, columns: Int) -> Matrix<T> {
         assert(rows * columns == self.rows * self.columns, "Element count must be unchanged.")
         
         return Matrix(rows: rows, columns: columns, elements: self.elements)
