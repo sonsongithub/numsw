2d70aeb49738c038da5a3da308ce94e90239905c
araki <araki@qoncept.co.jp>
2017-03-13 17:18:46 +0900

    Make MatrixInvert generic

---
+++ Sources/numsw/Common/FloatDouble.swift
@@ -0,0 +1,20 @@
+
+public protocol FloatProtocol {
+    var value: Float { get }
+}
+
+public protocol DoubleProtocol {
+    var value: Double { get }
+}
+
+extension Float: FloatProtocol {
+    public var value: Float {
+        return self
+    }
+}
+
+extension Double: DoubleProtocol {
+    public var value: Double {
+        return self
+    }
+}
--- Sources/numsw/Matrix/MatrixInvert.swift
+++ Sources/numsw/Matrix/MatrixInvert.swift
@@ -1,14 +1,45 @@
-//import Accelerate
-//
-//extension Matrix {
-//    public func inverted() -> Matrix {
-//        var inMatrix: [Double] = self.elements
-//        var N: __CLPK_integer        = __CLPK_integer( sqrt( Double( self.rows*self.columns ) ) )
-//        var pivots: [__CLPK_integer] = [__CLPK_integer](repeating: 0, count: Int(N))
-//        var workspace: [Double]      = [Double](repeating: 0.0, count: Int(N))
-//        var error: __CLPK_integer   = 0
-//        dgetrf_(&N, &N, &inMatrix, &N, &pivots, &error)
-//        dgetri_(&N, &inMatrix, &N, &pivots, &workspace, &N, &error)
-//        return Matrix(rows: self.rows, columns: self.columns, elements: inMatrix)
-//    }
-//}
+
+
+#if os(iOS) || os(OSX)
+
+    import Accelerate
+    
+    extension Matrix where T: DoubleProtocol {
+        public func inverted() -> Matrix<Double> {
+            precondition(rows == columns, "Matrix is not square.")
+            
+            var inMatrix: [Double] = self.elements.map { $0.value }
+            var N: __CLPK_integer        = __CLPK_integer( sqrt( Double( self.rows*self.columns ) ) )
+            var pivots: [__CLPK_integer] = [__CLPK_integer](repeating: 0, count: Int(N))
+            var workspace: [Double]      = [Double](repeating: 0.0, count: Int(N))
+            var error: __CLPK_integer   = 0
+            dgetrf_(&N, &N, &inMatrix, &N, &pivots, &error)
+            dgetri_(&N, &inMatrix, &N, &pivots, &workspace, &N, &error)
+
+            return Matrix<Double>(rows: rows,
+                                  columns: columns,
+                                  elements: inMatrix)
+            
+        }
+    }
+    
+    extension Matrix where T: FloatProtocol {
+        public func inverted() -> Matrix<Float> {
+            precondition(rows == columns, "Matrix is not square.")
+            
+            var inMatrix: [Float] = self.elements.map { $0.value }
+            var N: __CLPK_integer        = __CLPK_integer( sqrt( Double( self.rows*self.columns ) ) )
+            var pivots: [__CLPK_integer] = [__CLPK_integer](repeating: 0, count: Int(N))
+            var workspace: [Float]      = [Float](repeating: 0.0, count: Int(N))
+            var error: __CLPK_integer   = 0
+            sgetrf_(&N, &N, &inMatrix, &N, &pivots, &error)
+            sgetri_(&N, &inMatrix, &N, &pivots, &workspace, &N, &error)
+            
+            return Matrix<Float>(rows: rows,
+                                 columns: columns,
+                                 elements: inMatrix)
+            
+        }
+    }
+    
+#endif
