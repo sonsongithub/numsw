413976bbe7d25c10c12b814a4d9fc6d8fff9cef0
araki <araki@qoncept.co.jp>
2017-03-13 16:58:55 +0900

    Make MatrixNorm generic

--- Sources/numsw/Matrix/MatrixNorm.swift
+++ Sources/numsw/Matrix/MatrixNorm.swift
@@ -1,23 +1,16 @@
-////
-////  Matrix_sonson.swift
-////  numsw
-////
-////  Created by sonson on 2017/03/04.
-////
-////
-//
-//import Foundation
-//
-//public func frobeniusNorm(_ hs: Matrix) -> Double {
-//    return hs.elements.reduce(0) { (result, value) -> Double in
-//        return result + value * value
-//    }
-//}
-//
-//extension Matrix {
-//    public var frobeniusNorm: Double {
-//        return self.elements.reduce(0) { (result, value) -> Double in
-//            return result + value * value
-//        }
-//    }
-//}
+
+import Foundation
+
+public func frobeniusNorm<T: FloatingPoint>(_ hs: Matrix<T>) -> T {
+    return hs.elements.reduce(0) { (result, value) in
+        return result + value * value
+    }
+}
+
+extension Matrix where T: FloatingPoint {
+    public var frobeniusNorm: T {
+        return self.elements.reduce(0) { (result, value)  in
+            return result + value * value
+        }
+    }
+}
