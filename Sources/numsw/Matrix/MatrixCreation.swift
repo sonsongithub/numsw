e42d6644f66dc4c7c6f8260b289a787c74fc6b62
araki <araki@qoncept.co.jp>
2017-03-13 11:56:51 +0900

    Make MatrixCreation generic

--- Sources/numsw/Matrix/MatrixCreation.swift
+++ Sources/numsw/Matrix/MatrixCreation.swift
@@ -1,32 +1,40 @@
 
-//extension Matrix {
-//    public static func zeros(rows: Int, columns: Int) -> Matrix {
-//        let count = columns * rows
-//        let cElements = UnsafeMutablePointer<Double>.allocate(capacity: count)
-//        defer { cElements.deallocate(capacity: count) }
-//        for i in 0..<count {
-//            cElements[i] = Double(0)
-//        }
-//        return Matrix(rows: rows, columns: columns,
-//                      elements: Array(UnsafeBufferPointer(start: cElements, count: count)))
-//    }
-//    
-//    public static func ones(rows: Int, columns: Int) -> Matrix {
-//        let count = columns * rows
-//        let cElements = UnsafeMutablePointer<Double>.allocate(capacity: count)
-//        defer { cElements.deallocate(capacity: count) }
-//        for i in 0..<count {
-//            cElements[i] = Double(1)
-//        }
-//        return Matrix(rows: rows, columns: columns,
-//                      elements: Array(UnsafeBufferPointer(start: cElements, count: count)))
-//    }
-//    
-//    public static func eye(size: Int) -> Matrix {
-//        var matrix = Matrix.zeros(rows: size, columns: size)
-//        for i in 0..<size {
-//            matrix.elements[i + i * matrix.columns] = 1
-//        }
-//        return matrix
-//    }
-//}
+
+extension Matrix {
+    public static func filled(with value: T, rows: Int, columns: Int) -> Matrix<T> {
+        let elements = [T](repeating: value, count: rows*columns)
+        return Matrix(rows: rows, columns: columns, elements: elements)
+    }
+}
+
+extension Matrix where T: ZeroOne {
+    public static func zeros(rows: Int, columns: Int) -> Matrix<T> {
+        return Matrix.filled(with: T.zero, rows: rows, columns: columns)
+    }
+    
+    public static func ones(rows: Int, columns: Int) -> Matrix<T> {
+        return Matrix.filled(with: T.one, rows: rows, columns: columns)
+    }
+    
+    public static func eye(_ size: Int) -> Matrix<T> {
+        var identity = zeros(rows: size, columns: size)
+        (0..<size).forEach { identity[$0, $0] = T.one }
+        return identity
+    }
+}
+
+extension Matrix where T: Strideable {
+    public static func range(from: T, to: T, stride: T.Stride) -> Matrix<T> {
+        let elements = Array(Swift.stride(from: from, to: to, by: stride))
+        return Matrix(rows: 1, columns: elements.count, elements: elements)
+    }
+}
+
+extension Matrix where T: FloatingPoint {
+    public static func linspace(low: T, high: T, count: Int) -> Matrix<T> {
+        let d = high - low
+        let steps = T(count-1)
+        let elements = (0..<count).map { low + T($0)*d/steps }
+        return Matrix(rows: 1, columns: count, elements: elements)
+    }
+}
