f160b9db7299540bf1c66be452d0e2a398d7f816
araki <araki@qoncept.co.jp>
2017-03-14 10:24:08 +0900

    Add operator tests

--- Tests/numswTests/NDArrayTests/NDArrayArithmeticTests.swift
+++ Tests/numswTests/NDArrayTests/NDArrayArithmeticTests.swift
@@ -15,6 +15,17 @@ class NDArrayArithmeticTests: XCTestCase {
         }
     }
     
+    func testUnaryPlusOperator() {
+        do {
+            let a = NDArray<Int>(shape: [2, 2], elements: [1, 2, 3, 4])
+            XCTAssertEqual(+a, a)
+        }
+        do {
+            let a = NDArray<Double>(shape: [2, 2], elements: [1, 2, 3, 4])
+            XCTAssertEqualWithAccuracy(+a, a)
+        }
+    }
+    
     func testUnaryMinus() {
         do {
             let a = NDArray<Int>(shape: [2, 2], elements: [1, 2, 3, 4])
@@ -23,6 +34,18 @@ class NDArrayArithmeticTests: XCTestCase {
         do {
             let a = NDArray<Double>(shape: [2, 2], elements: [1, 2, 3, 4])
             XCTAssertEqualWithAccuracy(unaryMinus(a),
+                                       NDArray(shape: [2, 2], elements: [-1, -2, -3, -4]))
+        }
+    }
+    
+    func testUnaryMinusOperator() {
+        do {
+            let a = NDArray<Int>(shape: [2, 2], elements: [1, 2, 3, 4])
+            XCTAssertEqual(-a, NDArray(shape: [2, 2], elements: [-1, -2, -3, -4]))
+        }
+        do {
+            let a = NDArray<Double>(shape: [2, 2], elements: [1, 2, 3, 4])
+            XCTAssertEqualWithAccuracy(-a,
                                        NDArray(shape: [2, 2], elements: [-1, -2, -3, -4]))
         }
     }
@@ -55,6 +78,34 @@ class NDArrayArithmeticTests: XCTestCase {
         }
     }
     
+    func testAddOperator() {
+        // NDArray and scalar
+        do {
+            let a = NDArray<Int>(shape: [2, 2], elements: [1, 2, 3, 4])
+            XCTAssertEqual(a + 1, NDArray(shape: [2, 2], elements: [2, 3, 4, 5]))
+            XCTAssertEqual(1 + a, NDArray(shape: [2, 2], elements: [2, 3, 4, 5]))
+        }
+        do {
+            let a = NDArray<Double>(shape: [2, 2], elements: [1, 2, 3, 4])
+            XCTAssertEqualWithAccuracy(a + 1,
+                                       NDArray(shape: [2, 2], elements: [2, 3, 4, 5]))
+            XCTAssertEqualWithAccuracy(1 + a,
+                                       NDArray(shape: [2, 2], elements: [2, 3, 4, 5]))
+        }
+        
+        // NDArray and NDArray
+        do {
+            let a = NDArray<Int>(shape: [2, 2], elements: [1, 2, 3, 4])
+            let b = NDArray<Int>(shape: [2, 2], elements: [-1, 0, 1, 0])
+            XCTAssertEqual(a + b, NDArray(shape: [2, 2], elements: [0, 2, 4, 4]))
+        }
+        do {
+            let a = NDArray<Double>(shape: [2, 2], elements: [1, 2, 3, 4])
+            let b = NDArray<Double>(shape: [2, 2], elements: [-1, 0, 1, 0])
+            XCTAssertEqualWithAccuracy(a + b, NDArray(shape: [2, 2], elements: [0, 2, 4, 4]))
+        }
+    }
+    
     func testSubtract() {
         // NDArray and scalar
         do {
@@ -78,6 +129,32 @@ class NDArrayArithmeticTests: XCTestCase {
             let a = NDArray<Double>(shape: [2, 2], elements: [1, 2, 3, 4])
             let b = NDArray<Double>(shape: [2, 2], elements: [-1, 0, 1, 0])
             XCTAssertEqualWithAccuracy(subtract(a, b), NDArray(shape: [2, 2], elements: [2, 2, 2, 4]))
+        }
+    }
+    
+    func testSubtractOperator() {
+        // NDArray and scalar
+        do {
+            let a = NDArray<Int>(shape: [2, 2], elements: [1, 2, 3, 4])
+            XCTAssertEqual(a - 1, NDArray(shape: [2, 2], elements: [0, 1, 2, 3]))
+            XCTAssertEqual(1 - a, NDArray(shape: [2, 2], elements: [0, -1, -2, -3]))
+        }
+        do {
+            let a = NDArray<Double>(shape: [2, 2], elements: [1, 2, 3, 4])
+            XCTAssertEqualWithAccuracy(a - 1, NDArray(shape: [2, 2], elements: [0, 1, 2, 3]))
+            XCTAssertEqualWithAccuracy(1 - a, NDArray(shape: [2, 2], elements: [0, -1, -2, -3]))
+        }
+        
+        // NDArray and NDArray
+        do {
+            let a = NDArray<Int>(shape: [2, 2], elements: [1, 2, 3, 4])
+            let b = NDArray<Int>(shape: [2, 2], elements: [-1, 0, 1, 0])
+            XCTAssertEqual(a - b, NDArray(shape: [2, 2], elements: [2, 2, 2, 4]))
+        }
+        do {
+            let a = NDArray<Double>(shape: [2, 2], elements: [1, 2, 3, 4])
+            let b = NDArray<Double>(shape: [2, 2], elements: [-1, 0, 1, 0])
+            XCTAssertEqualWithAccuracy(a - b, NDArray(shape: [2, 2], elements: [2, 2, 2, 4]))
         }
     }
     
@@ -107,6 +184,32 @@ class NDArrayArithmeticTests: XCTestCase {
         }
     }
     
+    func testMultiplyOperator() {
+        // NDArray and scalar
+        do {
+            let a = NDArray<Int>(shape: [2, 2], elements: [1, 2, 3, 4])
+            XCTAssertEqual(a * 2, NDArray(shape: [2, 2], elements: [2, 4, 6, 8]))
+            XCTAssertEqual(2 * a, NDArray(shape: [2, 2], elements: [2, 4, 6, 8]))
+        }
+        do {
+            let a = NDArray<Double>(shape: [2, 2], elements: [1, 2, 3, 4])
+            XCTAssertEqualWithAccuracy(a * 2, NDArray(shape: [2, 2], elements: [2, 4, 6, 8]))
+            XCTAssertEqualWithAccuracy(2 * a, NDArray(shape: [2, 2], elements: [2, 4, 6, 8]))
+        }
+        
+        // NDArray and NDArray
+        do {
+            let a = NDArray<Int>(shape: [2, 2], elements: [1, 2, 3, 4])
+            let b = NDArray<Int>(shape: [2, 2], elements: [-1, 0, 2, 1])
+            XCTAssertEqual(a * b, NDArray(shape: [2, 2], elements: [-1, 0, 6, 4]))
+        }
+        do {
+            let a = NDArray<Double>(shape: [2, 2], elements: [1, 2, 3, 4])
+            let b = NDArray<Double>(shape: [2, 2], elements: [-1, 0, 2, 1])
+            XCTAssertEqualWithAccuracy(a * b, NDArray(shape: [2, 2], elements: [-1, 0, 6, 4]))
+        }
+    }
+    
     func testDivide() {
         // NDArray and scalar
         do {
@@ -133,6 +236,32 @@ class NDArrayArithmeticTests: XCTestCase {
         }
     }
     
+    func testDivideOperator() {
+        // NDArray and scalar
+        do {
+            let a = NDArray<Int>(shape: [2, 2], elements: [1, 2, 3, 4])
+            XCTAssertEqual(a / 2, NDArray(shape: [2, 2], elements: [0, 1, 1, 2]))
+            XCTAssertEqual(2 / a, NDArray(shape: [2, 2], elements: [2, 1, 0, 0]))
+        }
+        do {
+            let a = NDArray<Double>(shape: [2, 2], elements: [1, 2, 3, 4])
+            XCTAssertEqualWithAccuracy(a / 2, NDArray(shape: [2, 2], elements: [0.5, 1.0, 1.5, 2.0]))
+            XCTAssertEqualWithAccuracy(2 / a, NDArray(shape: [2, 2], elements: [2.0, 1.0, 2.0/3.0, 0.5]))
+        }
+        
+        // NDArray and NDArray
+        do {
+            let a = NDArray<Int>(shape: [2, 2], elements: [1, 2, 3, 4])
+            let b = NDArray<Int>(shape: [2, 2], elements: [-1, 2, 6, 1])
+            XCTAssertEqual(a / b, NDArray(shape: [2, 2], elements: [-1, 1, 0, 4]))
+        }
+        do {
+            let a = NDArray<Double>(shape: [2, 2], elements: [1, 2, 3, 4])
+            let b = NDArray<Double>(shape: [2, 2], elements: [-1, 2, 6, 1])
+            XCTAssertEqualWithAccuracy(a / b, NDArray(shape: [2, 2], elements: [-1, 1, 0.5, 4]))
+        }
+    }
+    
     func testModulo() {
         // NDArray and scalar
         do {
@@ -146,6 +275,22 @@ class NDArrayArithmeticTests: XCTestCase {
             let a = NDArray<Int>(shape: [2, 2], elements: [1, 2, 3, 4])
             let b = NDArray<Int>(shape: [2, 2], elements: [3, 2, 2, 3])
             XCTAssertEqual(modulo(a, b), NDArray(shape: [2, 2], elements: [1, 0, 1, 1]))
+        }
+    }
+    
+    func testModuloOperator() {
+        // NDArray and scalar
+        do {
+            let a = NDArray<Int>(shape: [2, 2], elements: [1, 2, 3, 4])
+            XCTAssertEqual(a % 2, NDArray(shape: [2, 2], elements: [1, 0, 1, 0]))
+            XCTAssertEqual(2 % a, NDArray(shape: [2, 2], elements: [0, 0, 2, 2]))
+        }
+        
+        // NDArray and NDArray
+        do {
+            let a = NDArray<Int>(shape: [2, 2], elements: [1, 2, 3, 4])
+            let b = NDArray<Int>(shape: [2, 2], elements: [3, 2, 2, 3])
+            XCTAssertEqual(a % b, NDArray(shape: [2, 2], elements: [1, 0, 1, 1]))
         }
     }
     
@@ -259,12 +404,19 @@ class NDArrayArithmeticTests: XCTestCase {
     static var allTests: [(String, (NDArrayArithmeticTests) -> () throws -> Void)] {
         return [
             ("testUnaryPlus", testUnaryPlus),
+            ("testUnaryPlusOperator", testUnaryPlusOperator),
             ("testUnaryMinus", testUnaryMinus),
+            ("testUnaryMinusOperator", testUnaryMinusOperator),
             ("testAdd", testAdd),
+            ("testAddOperator", testAddOperator),
             ("testSubtract", testSubtract),
+            ("testSubtractOperator", testSubtractOperator),
             ("testMultiply", testMultiply),
+            ("testMultiplyOperator", testMultiplyOperator),
             ("testDivide", testDivide),
+            ("testDivideOperator", testDivideOperator),
             ("testModulo", testModulo),
+            ("testModuloOperator", testModuloOperator),
         ]
     }
 }
