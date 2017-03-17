7f799086ab93d85deb49c151776273ee058ea668
araki <araki@qoncept.co.jp>
2017-03-17 20:19:55 +0900

    Fix: arc4random_uniform for linux

--- Sources/numsw/Common/Utils.swift
+++ Sources/numsw/Common/Utils.swift
@@ -1,6 +1,21 @@
 
+#if os(Linux)
+    import Glibc
+    import SwiftShims
+#else
+    import Darwin
+#endif
+
+func cs_arc4random_uniform(_ upperBound : UInt32 = UINT32_MAX) -> UInt32 {
+    #if os(Linux)
+        return _swift_stdlib_cxx11_mt19937_uniform(upperBound)
+    #else
+        return arc4random_uniform(upperBound)
+    #endif
+}
+
 import Foundation
 
 func _uniform<T: FloatingPoint>(low: T = 0, high: T = 1) -> T {
-    return (high-low)*(T(arc4random_uniform(UInt32.max)) / T(UInt32.max))+low
+    return (high-low)*(T(cs_arc4random_uniform(UInt32.max)) / T(UInt32.max))+low
 }
