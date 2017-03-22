e09bc6b5db9a9a0c593482173c82c1f5f755acb0
araki <araki@qoncept.co.jp>
2017-03-22 11:21:01 +0900

    Make Matrix elements internal(set)

--- Sources/numsw/Matrix/Matrix.swift
+++ Sources/numsw/Matrix/Matrix.swift
@@ -6,7 +6,7 @@ public struct Matrix<T> {
         return columns * rows
     }
     
-    public var elements: [T]
+    public internal(set) var elements: [T]
     
     public init(rows: Int, columns: Int, elements: [T]) {
         self.rows = rows
