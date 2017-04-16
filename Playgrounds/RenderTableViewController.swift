bb92ed9f97451dd4e8d22748dcd20fb6c924442a
Yusuke Ito <novi.mad@gmail.com>
2017-04-16 20:26:08 +0900

    file layout refactoring

--- Playgrounds/RenderTableViewController.swift
+++ Playgrounds/RenderTableViewController.swift
@@ -12,56 +12,9 @@
 import UIKit
 import QuartzCore
 
-    
 #if SANDBOX_APP
     //  sandbox app
-    internal enum PlaygroundValue {
-        case array([PlaygroundValue])
-        case boolean(Bool)
-        case data(Data)
-        case date(Date)
-        case dictionary([String: PlaygroundValue])
-        case floatingPoint(Double)
-        case integer(Int)
-        case string(String)
-    }
-    internal class PlaygroundKeyValueStore {
-        static let current = PlaygroundKeyValueStore()
-        private init() {
-            
-        }
-        subscript(key: String) -> PlaygroundValue? {
-            set {
-                print("KeyValueStore setting \(newValue as Any) for \(key)")
-                if let value = newValue {
-                    switch value {
-                    case .floatingPoint(let double):
-                        UserDefaults.standard.set(double, forKey: key)
-                    case .integer(let integer):
-                        UserDefaults.standard.set(integer, forKey: key)
-                    default:
-                        fatalError("Not implemented")
-                    }
-                } else {
-                    UserDefaults.standard.removeObject(forKey: key)
-                }
-            }
-            get {
-                print("KeyValueStore getting for \(key)")
-                guard let object = UserDefaults.standard.object(forKey: key) else {
-                    return nil
-                }
-                if let integer = object as? Int {
-                    return .integer(integer)
-                } else if let double = object as? Double {
-                    return .floatingPoint(double)
-                } else {
-                    fatalError("Not implemented")
-                }
-            }
-        }
-    }
-    #else
+#else
     //  playground
     import PlaygroundSupport
 #endif
@@ -233,9 +186,9 @@ public class RenderTableViewController: UITableViewController, UZTextViewDelegat
     }
     
     #if SANDBOX_APP
-    // Tap to dismiss any table view cell
+    // Tap any table view cell to dismiss
     public override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        self.dismiss(animated: true, completion: nil)
+        self.navigationController?.popViewController(animated: true)
     }
     #endif
     
